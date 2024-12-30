14c3e1acd7295523b1588c48b17178ad74c729eb
ghost <hibesofware@gmail.com>
2024-12-29 23:31:19 -0400

    added navigation

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function About() {
     return (
         <div>
             <h1>About Page</h1>
+            <Link href="/home">Home Page</Link>
+            <Link href="/">Default Page</Link>
         </div>
     );
 }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function HomePage() {
     return (
         <div>
             <h1>Home page</h1>
+            <Link href="/about">About</Link>
+            <Link href="/">Default Page</Link>
         </div>
     );
 }
