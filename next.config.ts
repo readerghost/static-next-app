1033b5b5a1b324dc3845a9d64894a2374bdb08e7
ghost <hibesofware@gmail.com>
2024-12-29 22:38:26 -0400

    removed basepath

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
    output: "export",
-   basePath: "/nextjs-github-pages",
+  //  basePath: "/nextjs-github-pages", 
    images: {
     unoptimized: true,
   },
